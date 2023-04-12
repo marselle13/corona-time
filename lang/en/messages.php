c0a84d5787ce1fe8891cd409a8e8b8bca5e817d2
marselle13 <nika.gogua8@gmail.com>
2023-04-12 22:35:12 +0400

    style: login page translation

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -1,10 +1,20 @@
 <?php
 
 return [
+	#registration page
 	'welcome_register'       => 'Welcome back',
 	'info_register'          => 'Please enter required info to sign up',
+	'have_account'           => 'Already have an account? ',
+	#login page
+	'welcome_login'          => 'Welcome back',
+	'info_login'             => 'Welcome back! Please enter your details',
+	'forget_password'        => 'Forget Password?',
+	'no_account'             => 'Don\'t have and an account? ',
+	'sign_free'              => 'Sign up for free',
+	#form
 	'username'               => 'Username',
 	'username_rule'          => 'Username should be unique, min 3 symbols',
+	'login_placeholder'      => 'Enter unique username or email',
 	'email'                  => 'Email',
 	'password'               => 'Password',
 	'repeat_password'        => 'Repeat Password',
@@ -13,6 +23,5 @@ return [
 	'password_placeholder'   => 'შეიყვანეთ პაროლი',
 	'remember_device'        => 'Remember this device',
 	'sign_up'                => 'Sign Up',
-	'have_account'           => 'Already have an account? ',
 	'log_in'                 => 'Log In',
 ];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,17 @@
 <x-layout>
     <div class="flex md:justify-between justify-center">
         <x-form.container class="lg:mx-28">
-            <x-form.auth info="Welcome back! Please enter your details">Welcome back</x-form.auth>
+            <x-form.auth info="{{trans('messages.info_login')}}">{{trans('messages.welcome_login')}}</x-form.auth>
             <form action="#" class="lg:max-w-[392px] md:space-y-6 space-y-4">
-                <x-form.input name="username" placeholder="Enter uniqure username or email"/>
-                <x-form.input name="password" type="password" placeholder="Fill in password"/>
+                <x-form.input name="{{trans('messages.username')}}" placeholder="{{trans('messages.login_placeholder')}}"/>
+                <x-form.input name="{{trans('messages.password')}}" type="password" placeholder="{{trans('messages.password_placeholder')}}"/>
                 <div class="flex justify-between gap-2">
                     <x-form.checkbox/>
-                    <a href="{{route('auth.reset_password')}}" class="text-[#2029F3] text-sm font-semibold">Forget Password?</a>
+                    <a href="{{route('auth.reset_password')}}" class="text-[#2029F3] text-sm font-semibold">{{trans('messages.forget_password')}}</a>
                 </div>
-                <x-form.button>Log In</x-form.button>
-                <div class="text-center"><p class="text-[#808189] text-sm md:text-base">Don't have and an account? <a
-                            href="{{route('auth.register')}}" class="text-[#010414] font-bold">Sign up for free</a></p>
+                <x-form.button>{{trans('messages.log_in')}}</x-form.button>
+                <div class="text-center"><p class="text-[#808189] text-sm md:text-base">{{trans('messages.no_account')}} <a
+                            href="{{route('auth.register')}}" class="text-[#010414] font-bold">{{trans('messages.sign_free')}}</a></p>
                 </div>
             </form>
         </x-form.container>
