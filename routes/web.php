1f02d2c6baf304b023e0e476049b343b4b47c17d
marselle13 <nika.gogua8@gmail.com>
2023-04-12 15:47:14 +0400

    chore: change route name

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/login', 'auth.login')->name('auth.login');
+Route::view('/login/recover-password', 'auth.recover-password')->name('auth.recover_password');
+Route::view('/login/reset-password', 'auth.reset-password')->name('auth.reset_password');
+Route::view('/login/new-password', 'auth.new-password')->name('auth.new_password');
+Route::view('/login/success-update', 'auth.success-update')->name('auth.success_update');
 Route::view('/register', 'auth.register')->name('auth.register');
-Route::view('/reset-password', 'auth.reset-password')->name('auth.reset_password');
-Route::view('/new-password', 'auth.new-password')->name('auth.new_password');
-Route::view('/success-update', 'auth.success-update')->name('auth.success_update');
-Route::view('/success-confirmation', 'auth.success-confirmation')->name('auth.success_confirmation');
-Route::view('/confirmation-email', 'auth.confirmation-email')->name('auth.confirmation_email');
-Route::view('/recover-password', 'auth.recover-password')->name('auth.recover_password');
+Route::view('register/confirmation-email', 'auth.confirmation-email')->name('auth.confirmation_email');
+Route::view('/register/success-confirmation', 'auth.success-confirmation')->name('auth.success_confirmation');
