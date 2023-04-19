9dcb729dc17b59b0bd7b547397c0de5a2a62aab1
marselle13 <nika.gogua8@gmail.com>
2023-04-19 13:39:21 +0400

    chore: remove session for verify token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\auth\RegisterRequest;
 use App\Models\User;
 use App\Notifications\VerifyEmailNotification;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\View\View;
 
@@ -15,18 +14,16 @@ class AuthController extends Controller
 	public function register(RegisterRequest $request, VerifyEmailNotification $verify): RedirectResponse
 	{
 		$user = User::create($request->except('password_confirmation'));
-		$request->session()->put('verify_token', $request->verify_token);
 		Notification::send($user, $verify);
 		return redirect(route('auth.success_registration', $user->id));
 	}
 
-	public function confirmation(Request $request, string $token): View
+	public function confirmation(string $token): View
 	{
 		$user = User::where('verify_token', $token)->firstOrFail();
 		$user->markEmailAsVerified();
 		$user->verify_token = null;
 		$user->save();
-		$request->session()->forget('verify_token');
 		return view('auth.success-confirmation');
 	}
 }
