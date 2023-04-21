cdd057f16154001eabfa65f89091022c9721e7e2
marselle13 <nika.gogua8@gmail.com>
2023-04-21 13:23:32 +0400

    chore: change route

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Route::middleware('guest')->group(function () {
 	Route::view('/login', 'auth.login-page')->name('auth.login_page');
 	Route::view('/login/reset-password', 'auth.reset-password')->name('auth.reset_page');
 	Route::view('/login/success-recover/{id}', 'auth.success-recover')->middleware('verify.change')->name('auth.success_recover');
-	Route::view('/login/success-update', 'auth.success-update')->name('auth.success_update');
 	Route::view('register', 'auth.register-page')->name('auth.register_page');
 	Route::view('register/success-registration/{id}', 'auth.success-registration')->middleware('verify.registration')->name('auth.success_registration');
 });
@@ -37,9 +36,9 @@ Route::controller(AuthController::class)->middleware('guest')->group(function ()
 });
 
 Route::controller(ResetPasswordController::class)->middleware('guest')->group(function () {
-	Route::post('/login/reset-password', 'reset')->name('auth.reset_password');
-	Route::get('/login/new-password/{token}', 'newPassword')->name('auth.new_password');
-	Route::patch('/login/new-password/{token}', 'updatePassword')->name('auth.update_password');
+	Route::post('/reset-password', 'reset')->name('auth.reset_password');
+	Route::get('/new-password/{token}', 'newPassword')->name('auth.new_password');
+	Route::patch('/new-password/{token}', 'updatePassword')->name('auth.update_password');
 });
 
 Route::controller(VerifyEmailController::class)->middleware('guest')->group(function () {
