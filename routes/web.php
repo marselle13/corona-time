f2b5cb36a6b5b8758f4fa6e5627dd446a61bbf07
marselle13 <nika.gogua8@gmail.com>
2023-04-22 17:53:58 +0400

    chore: minor changes * change verify middleware name * change register route placment

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,12 @@ Route::post('/language', [LanguageController::class, 'setLanguage'])->name('lang
 Route::view('/', 'landing')->middleware(['auth', 'verified'])->name('landing.worldwide');
 
 Route::middleware('guest')->group(function () {
+	Route::view('/register', 'auth.register-page')->name('auth.register_page');
+	Route::view('/register/success-registration/', 'success.registration')->name('successes.registration');
 	Route::view('/login', 'auth.login-page')->name('auth.login_page');
 	Route::view('/reset-password', 'password.reset')->name('passwords.reset_page');
 	Route::view('/success-recover', 'success.password-recover')->name('successes.password_recover');
-	Route::view('/new-password/{userId}/{token}', 'password.update-page')->middleware(['checkVerified', 'verify'])->name('passwords.update_page');
-	Route::view('/register', 'auth.register-page')->name('auth.register_page');
-	Route::view('/register/success-registration/', 'success.registration')->name('successes.registration');
+	Route::view('/new-password/{userId}/{token}', 'password.update-page')->middleware(['checkVerified', 'verifyToken'])->name('passwords.update_page');
 });
 
 Route::controller(AuthController::class)->middleware('guest')->group(function () {
@@ -38,9 +38,9 @@ Route::controller(AuthController::class)->middleware('guest')->group(function ()
 
 Route::controller(ResetPasswordController::class)->middleware(['guest', 'checkVerified'])->group(function () {
 	Route::post('/reset-password', 'resetPassword')->name('passwords.reset');
-	Route::patch('/new-password/{userId}/{token}', 'updatePassword')->middleware('verify')->name('passwords.update');
+	Route::patch('/new-password/{userId}/{token}', 'updatePassword')->middleware('verifyToken')->name('passwords.update');
 });
 
-Route::controller(VerifyEmailController::class)->middleware(['guest', 'verify'])->group(function () {
+Route::controller(VerifyEmailController::class)->middleware(['guest', 'verifyToken'])->group(function () {
 	Route::get('/confirmation-email/{userId}/{token}', 'confirmation')->name('emails.confirmation');
 });
