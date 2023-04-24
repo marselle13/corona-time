26cb0f4d2c77a51dc4b8689950f819f4feb821da
marselle13 <nika.gogua8@gmail.com>
2023-04-24 14:23:56 +0400

    chore: change schedule time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
 	 */
 	protected function schedule(Schedule $schedule): void
 	{
-		$schedule->command('fetch:covid-data')->everyMinute();
+		$schedule->command('fetch:covid-data')->daily();
 	}
 
 	/**
