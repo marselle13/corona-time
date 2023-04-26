5eaf1c11df6fc4591af44eaeb21635aa7d6d94de
marselle13 <nika.gogua8@gmail.com>
2023-04-26 11:33:19 +0400

    fix: card background color problem

--- resources/views/landing/worldwide.blade.php
+++ resources/views/landing/worldwide.blade.php
@@ -3,9 +3,9 @@
     @include('landing._navigation' , ['head' => trans('messages.world_statistic')])
     <main class="md:w-[85%] mx-auto grid lg:grid-cols-3 gap-6 mt-6 md:mt-10 grid-cols-2 w-[96%]">
         @if($worldwide)
-        <x-card color="#2029F3" icon="high" class="col-span-2 lg:col-span-1" head="{{trans('messages.new_cases')}}">{{$worldwide->confirmed}}</x-card>
-        <x-card color="#0FBA68" icon="low" head="{{trans('messages.recovered')}}">{{$worldwide->recovered}}</x-card>
-        <x-card color="#EAD621" icon="middle" head="{{trans('messages.death')}}">{{$worldwide->deaths}}</x-card>
+        <x-card color="#2029F3" icon="high" class="bg-[#2029F3] text-[#2029F3] col-span-2 lg:col-span-1" head="{{trans('messages.new_cases')}}">{{$worldwide->confirmed}}</x-card>
+        <x-card color="#0FBA68" icon="low" class="bg-[#0FBA68] text-[#0FBA68] " head="{{trans('messages.recovered')}}">{{$worldwide->recovered}}</x-card>
+        <x-card color="#EAD621" icon="middle" class="" class="bg-[#EAD621] text-[#EAD621]" head="{{trans('messages.death')}}">{{$worldwide->deaths}}</x-card>
         @else
         <p>{{trans('messages.no_data')}}</p>
         @endif
