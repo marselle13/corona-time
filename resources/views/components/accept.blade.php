fce01f6bcc6f08bf47214d6583617f32968dc146
marselle13 <nika.gogua8@gmail.com>
2023-04-27 14:46:15 +0400

    style: class for media

--- resources/views/components/accept.blade.php
+++ resources/views/components/accept.blade.php
@@ -4,12 +4,10 @@
         .wrapper {
             margin: 0;
         }
-
         .text-div h1 {
             margin: 0
 
         }
-
         .button-text{
             width: auto;
         }
@@ -25,15 +23,15 @@
 </head>
 <html lang="en" style="font-family: 'inter', sans-serif; overflow-x: hidden">
 <body>
-<div style="max-width: 520px;  margin: 12rem auto; background: white;">
+<div class="wrapper" style="max-width: 520px;  margin: 12rem auto; background: white;">
     <img src="{{asset('./images/worldwide.png')}}" alt="worldwide" style="width: 100%"/>
-    <div style="margin: 0 64px;">>
-        <div style="text-align: center; margin-top: 3.5rem">
+    <div class="info" style="margin: 0 64px;">>
+        <div class="text-div" style="text-align: center; margin-top: 3.5rem">
             <h1 style="margin: 0 auto; font-weight: 900; font-size: 24px;">{{$slot}}</h1>
             <p style="margin: 0 auto; font-size: 20px; color: #808189;">{{$info}}</p>
         </div>
         <div style="text-align: center; margin-top: 2.5rem; background-color: #0FBA68; border-radius: 12px;">
-            <a style="text-align: center; text-decoration: none; font-weight: 900; color: white; text-transform: uppercase; display: inline-block; width: 100%; padding: 20px 0;" href="{{$link}}"><span>{{$button}}</span></a>
+            <a class="button-text" style="text-align: center; text-decoration: none; font-weight: 900; color: white; text-transform: uppercase; display: inline-block; width: 100%; padding: 20px 0;" zhref="{{$link}}"><span>{{$button}}</span></a>
         </div>
     </div>
 </div>
