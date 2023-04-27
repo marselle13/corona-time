2a430f0c1d844d83f3d1b95eacda1d1ab8e55411
marselle13 <nika.gogua8@gmail.com>
2023-04-27 14:44:04 +0400

    style: inline email notification styles

--- resources/views/components/accept.blade.php
+++ resources/views/components/accept.blade.php
@@ -1,63 +1,5 @@
 @props(['link','button','info'])
 <style>
-    html, body {
-        font-family: 'inter', sans-serif;
-        overflow-x: hidden;
-    }
-
-    img {
-        width: 100%;
-    }
-
-    .wrapper {
-        max-width: 520px;
-        margin: 12rem auto;
-        background: white;
-
-    }
-
-    .info {
-        margin: 0 64px;
-    }
-
-    .text-div {
-        text-align: center;
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        margin-top: 3.5rem;
-    }
-
-    .text-div h1 {
-        margin: 0 auto;
-        font-weight: 900;
-        font-size: 24px;
-    }
-
-    .text-div p {
-        margin: 0 auto;
-        font-size: 20px;
-        color: #808189;
-    }
-
-    .button {
-        text-align: center;
-        margin-top: 2.5rem;
-        background-color: #0FBA68;
-        border-radius: 12px;
-    }
-
-    .button-text {
-        text-align: center;
-        text-decoration: none;
-        font-weight: 900;
-        color: white;
-        text-transform: uppercase;
-        display: inline-block;
-        width: 100%;
-        padding: 20px 0;
-    }
-
     @media (max-width: 520px) {
         .wrapper {
             margin: 0;
@@ -81,17 +23,17 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <title>email</title>
 </head>
-<html lang="en">
+<html lang="en" style="font-family: 'inter', sans-serif; overflow-x: hidden">
 <body>
-<div class="wrapper">
-    <img src="{{asset('./images/worldwide.png')}}" alt="worldwide"/>
-    <div class="info">
-        <div class="text-div">
-            <h1>{{$slot}}</h1>
-            <p>{{$info}}</p>
+<div style="max-width: 520px;  margin: 12rem auto; background: white;">
+    <img src="{{asset('./images/worldwide.png')}}" alt="worldwide" style="width: 100%"/>
+    <div style="margin: 0 64px;">>
+        <div style="text-align: center; margin-top: 3.5rem">
+            <h1 style="margin: 0 auto; font-weight: 900; font-size: 24px;">{{$slot}}</h1>
+            <p style="margin: 0 auto; font-size: 20px; color: #808189;">{{$info}}</p>
         </div>
-        <div class="button">
-            <a class="button-text" href="{{$link}}"><span>{{$button}}</span></a>
+        <div style="text-align: center; margin-top: 2.5rem; background-color: #0FBA68; border-radius: 12px;">
+            <a style="text-align: center; text-decoration: none; font-weight: 900; color: white; text-transform: uppercase; display: inline-block; width: 100%; padding: 20px 0;" href="{{$link}}"><span>{{$button}}</span></a>
         </div>
     </div>
 </div>
