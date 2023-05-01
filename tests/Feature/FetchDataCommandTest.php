c26af85b5c95d3f982e11262be7bac61b1464dd7
marselle13 <nika.gogua8@gmail.com>
2023-05-01 13:01:54 +0400

    test: fake api data get

--- tests/Feature/FetchDataCommandTest.php
+++ tests/Feature/FetchDataCommandTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class FetchDataCommandTest extends TestCase
@@ -11,7 +12,18 @@ class FetchDataCommandTest extends TestCase
 
 	public function test_data_fetching_in_database(): void
 	{
+		// Set up fake data for countries endpoint
+		$fakeCountries = [
+			['code' => 'GE', 'name' => ['en' => 'Georgia', 'ka' => 'საქართველო']],
+		];
+		Http::fake([
+			'https://devtest.ge/countries' => Http::response($fakeCountries, 200),
+		]);
+
+		Http::post('https://devtest.ge/get-countries-statistics', ['code' => 'GE']);
+
 		$this->artisan('fetch:covid-data');
+
 		$this->assertDatabaseHas('statistics', [
 			'code'    => 'GE',
 			'country' => 'Georgia',
