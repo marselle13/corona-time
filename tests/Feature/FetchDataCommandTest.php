ec8dc98f5b87108655c2b51e848edb3109b4e25e
marselle13 <nika.gogua8@gmail.com>
2023-05-01 14:05:08 +0400

    fix: get fake statistics

--- tests/Feature/FetchDataCommandTest.php
+++ tests/Feature/FetchDataCommandTest.php
@@ -12,15 +12,24 @@ class FetchDataCommandTest extends TestCase
 
 	public function test_data_fetching_in_database(): void
 	{
-		// Set up fake data for countries endpoint
 		$fakeCountries = [
 			['code' => 'GE', 'name' => ['en' => 'Georgia', 'ka' => 'საქართველო']],
 		];
-		Http::fake([
-			'https://devtest.ge/countries' => Http::response($fakeCountries, 200),
-		]);
 
-		Http::post('https://devtest.ge/get-countries-statistics', ['code' => 'GE']);
+		$fakeStatistics = [
+			'country'    => 'Georgia',
+			'confirmed'  => 1,
+			'recovered'  => 1,
+			'critical'   => 1,
+			'deaths'     => 1,
+			'created_at' => now(),
+			'updated_at' => now(),
+		];
+
+		Http::fake([
+			'https://devtest.ge/countries'                => Http::response($fakeCountries, 200),
+			'https://devtest.ge/get-countries-statistics' => Http::response($fakeStatistics, 200),
+		]);
 
 		$this->artisan('fetch:covid-data');
 
